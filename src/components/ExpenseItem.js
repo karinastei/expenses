1c7af289c48730670e1f51b76dcf65f3c5960bcb
Karina <karina.steinberg@voco.ee>
2023-01-11 14:54:18 +0200

    Created expense components.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,11 +2,10 @@ import './ExpenseItem.css'
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props){
-
-    return (
+return (
         <div className="expense-item">
-            <ExpenseDate date={props.expenseData.date}></ExpenseDate>
-            <div className="expense-item__description">
+        <ExpenseDate date = {props.expenseData.date}></ExpenseDate>
+        <div className="expense-item__description">
                 <h2>{props.expenseData.title}</h2>
                 <div className="expense-item__price">{props.expenseData.price}€</div>
             </div>
