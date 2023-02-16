d65f863f2544038491587493ece5a40c2745f561
Karina <karina.steinberg@voco.ee>
2023-02-16 19:28:57 +0200

    Kasutan ExpenseItemContainer'it div'i asemel

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
 import './ExpenseItem.css'
 import ExpenseDate from "./ExpenseDate";
+import ExpenseItemContainer from "./ExpenseItemContainer";
 
 function ExpenseItem(props) {
     return (
-        <div className="expense-item">
+        <ExpenseItemContainer className="expense-item">
             <ExpenseDate date={props.expenseData.date}></ExpenseDate>
             <div className="expense-item__description">
                 <h2>{props.expenseData.title}</h2>
                 <div className="expense-item__price">{props.expenseData.price}€</div>
             </div>
-        </div>
+        </ExpenseItemContainer>
     )
 }
 
